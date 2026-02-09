220339bb3202ccdef38e0e575c4149ff34df00e6
Akshat-pandey4285 <akshatpandey4285@gmail.com>
2026-02-09 16:40:02 +0000

    change in devcontainer and create a new variable

---
+++ .devcontainer/Basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountID=1534345
+let accountEmail="akshat@gmail.com"
+var accountPassword="12345"
+accountCity="U.P"
+
+
+//accountID=2  //not allowed
+
+accountEmail="abc@gmail.com"
+accountPassword="abcd"
+accountCity="Delhi"
+
+
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
